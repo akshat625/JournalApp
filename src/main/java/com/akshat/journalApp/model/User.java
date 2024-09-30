f5a61d54bd8442dde2bc375c6513ad2973183309
Akshat Srivastava <akshatsrivastava625@gmail.com>
2024-09-30 21:20:39 +0530

    add users entity

--- src/main/java/com/akshat/journalApp/model/User.java
+++ src/main/java/com/akshat/journalApp/model/User.java
@@ -1,26 +1,27 @@
 package com.akshat.journalApp.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.time.LocalDateTime;
-
-
-@Document
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
-public class JournalEntry {
+@Document(collection = "users")
+public class User {
+
     @Id
     private ObjectId id;
+    @Indexed(unique = true)
     @NonNull
-    private String title;
-    private String content;
-    private LocalDateTime date;
+    private String userName;
+    @NonNull
+    private String password;
+
+
+
+
+
 
 }
