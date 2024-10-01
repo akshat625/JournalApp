b82a34772f9b649c6a24a31f8da6b7fd63e5d49d
Akshat Srivastava <akshatsrivastava625@gmail.com>
2024-10-01 14:18:20 +0530

    write user controller

--- src/main/java/com/akshat/journalApp/controller/UserController.java
+++ src/main/java/com/akshat/journalApp/controller/UserController.java
@@ -1,9 +1,38 @@
 package com.akshat.journalApp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.akshat.journalApp.model.User;
+import com.akshat.journalApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+
+    }
+
+    @PutMapping("/{username}")
+    public ResponseEntity<User> updateUser(@RequestBody User user, @PathVariable String username) {
+        return userService.updateUser(user, username);
+    }
+
+
+
+
+
 }
--- src/main/java/com/akshat/journalApp/service/UserService.java
+++ src/main/java/com/akshat/journalApp/service/UserService.java
@@ -4,6 +4,9 @@ import com.akshat.journalApp.model.User;
 import com.akshat.journalApp.repo.UserRepo;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,12 +17,19 @@ public class UserService {
     @Autowired
     private UserRepo userRepo;
 
-    public void saveUser(User user) {
-        userRepo.save(user);
+    public ResponseEntity<?> saveUser(User user) {
+        try {
+            userRepo.save(user);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatusCode.valueOf(403));
+        }
+
     }
 
-    public List<User> getAllUsers() {
-        return userRepo.findAll();
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userRepo.findAll(), HttpStatus.OK);
     }
 
     public User getUserById(ObjectId id) {
@@ -30,4 +40,27 @@ public class UserService {
         userRepo.deleteById(id);
     }
 
+    public User findByUserName(String username) {
+        return userRepo.findByUserName(username);
+    }
+
+    public ResponseEntity<User> updateUser(User user,String userName) {
+        try {
+            User userInDb = userRepo.findByUserName(userName);
+            if(userInDb != null){
+                userInDb.setUserName(user.getUserName());
+                userInDb.setPassword(user.getPassword());
+                userRepo.save(userInDb);
+                return new ResponseEntity<>(HttpStatus.OK);
+            }
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+
+
+    }
 }
